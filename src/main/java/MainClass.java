3d79cce2ab5ce8affd5b74e511fd33b1a206ecb5
Sergey Desyatkov <desyatkov@gmail.com>
2019-01-15 21:39:33 +0200

    init

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainClass {
+  public static void main(String[] args) {
+    System.setProperty("webdriver.gecko.driver", "/Users/sergey.desyatkov/Desktop/prod/seleniumtest/drivers/geckodriver");
+
+    WebDriver driver = new FirefoxDriver();
+    driver.get("http://google.com");
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.findElement(By.cssSelector("[role='combobox']")).sendKeys("twitter");
+    driver.findElement(By.cssSelector("[role='combobox']")).sendKeys(Keys.RETURN);
+    driver.findElement(By.cssSelector("a[href='https://twitter.com/?lang=he']")).click();
+
+    WebDriverWait wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[href*='signup']")));
+
+    String expectedTitle = "Sign up for Twitter / Twitter";
+
+    assertEquals(expectedTitle, driver.getTitle());
+
+    driver.quit();
+  }
+}
